e987b68ce63ad542ebfeba2297f269c09ce44004
AasmaKhan <aasma.ansari429@gmai.com>
2020-10-09 08:54:16 +0530

    first commit

---
+++ src/test/java/qaclickacademy/MavenJava/AppiumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void nativeApps()
+	{
+		System.out.println("nativeApps");
+	}
+	
+	@Test
+	public void iosApps()
+	{
+		System.out.println("iosApps");
+	}
+
+}
